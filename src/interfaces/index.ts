cba9c9ddd9da6a4421d5951b34fbd080206be2b1
Mateo <71zaps-memento@icloud.com>
2025-05-20 13:15:26 -0500

    feat: extend Movie and CastMember interfaces with additional properties and introduce new interfaces for GenresResponse, PaginatedResponse, MovieDetails, and TMDBConfiguration

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,14 +2,20 @@ export interface Movie {
 	id: number;
 	title: string;
 	overview: string;
-	poster_path: string;
-	backdrop_path: string;
+	poster_path: string | null;
+	backdrop_path: string | null;
 	release_date: string;
 	vote_average: number;
 	vote_count: number;
-	runtime?: number;
+	runtime?: number | null;
 	genres: Genre[];
-	cast?: CastMember[];
+	cast: CastMember[];
+	popularity?: number;
+	original_language?: string;
+	original_title?: string;
+	adult?: boolean;
+	video?: boolean;
+	genre_ids?: number[];
 }
 
 export interface Genre {
@@ -17,15 +23,73 @@ export interface Genre {
 	name: string;
 }
 
+export interface GenresResponse {
+	genres: Genre[];
+}
+
 export interface CastMember {
 	id: number;
 	name: string;
 	character: string;
 	profile_path: string | null;
+	adult?: boolean;
+	gender?: number | null;
+	known_for_department?: string;
+	original_name?: string;
+	popularity?: number;
+	cast_id?: number;
+	credit_id?: string;
+	order?: number;
 }
 
 export interface MovieCategory {
 	id: number;
 	name: string;
 	description: string;
+}
+
+export interface PaginatedResponse<T> {
+	page: number;
+	results: T[];
+	total_pages: number;
+	total_results: number;
+}
+
+export interface MovieDetails extends Movie {
+	belongs_to_collection?: object | null;
+	budget?: number;
+	homepage?: string | null;
+	imdb_id?: string | null;
+	production_companies?: {
+		name: string;
+		id: number;
+		logo_path: string | null;
+		origin_country: string;
+	}[];
+	production_countries?: {
+		iso_3166_1: string;
+		name: string;
+	}[];
+	revenue?: number;
+	spoken_languages?: {
+		english_name: string;
+		iso_639_1: string;
+		name: string;
+	}[];
+	status?: string;
+	tagline?: string | null;
+	cast: CastMember[];
+}
+
+export interface TMDBConfiguration {
+	images: {
+		base_url: string;
+		secure_base_url: string;
+		backdrop_sizes: string[];
+		logo_sizes: string[];
+		poster_sizes: string[];
+		profile_sizes: string[];
+		still_sizes: string[];
+	};
+	change_keys: string[];
 }
